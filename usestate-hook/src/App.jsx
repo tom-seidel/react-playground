9de3b60e0860ebd347ae0d188592e3a6f9a7a5f2
Tom Seidel <hi@tomseidel.com>
2025-06-09 22:19:33 +0200

    feat: add useState onClick name change

--- usestate-hook/src/App.jsx
+++ usestate-hook/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
+  const [name, setName] = useState('Tom');
+
+  const chaneName = () => {
+    setName('Jan');
+  }
 
   return (
     <>
-      <h1>useState Hook</h1>
+      <h1>{name}</h1>
+      <button onClick={chaneName}>Change Name</button>
     </>
   )
 }
