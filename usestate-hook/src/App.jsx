c10569639463de389040303bc70ada668de85c19
Tom Seidel <hi@tomseidel.com>
2025-06-10 17:26:50 +0200

    feat: add counter with decrement and increment buttons

--- usestate-hook/src/App.jsx
+++ usestate-hook/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
-
-
 function App() {
   const [name, setName] = useState('Tom');
-
+  const [counter, setCounter] = useState(0);
+  
 function changeName() {
   name == ('Tom') ? setName('Jan') : setName('Tom');
+}
+
+
+function decrement() {
+  setCounter(counter - 1);
+}
+
+function increment() {
+  setCounter(counter + 1);
 }
 
   return (
@@ -15,6 +23,10 @@ function changeName() {
       <h1>Change Name:</h1>
       <p>{name}</p>
       <button onClick={changeName}>Change Name</button>
+      <h1>Counter:</h1>
+      <p className='counter'>{counter}</p>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
     </>
   )
 }
