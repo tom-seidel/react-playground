d1ccb807cabfe0cdb66eb560bf50e2f2fb36035b
tom-seidel <hi@tomseidel.com>
2025-07-26 06:15:02 +0200

    feat: add third const as an example

--- jsx-syntax/src/App.jsx
+++ jsx-syntax/src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 const hello = 'Hello';
 const world = 'World';
 
+const element = <h1>{hello} {world}</h1>;
+
 function App() {
 
   return (
     <>
       <h1>{hello} {world}</h1>
+      {element}
     </>
   )
 }
