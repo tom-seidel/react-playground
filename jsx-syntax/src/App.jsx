82a2480e49e308a1ed5443c55959a6f4443fbdc8
tom-seidel <hi@tomseidel.com>
2025-07-27 05:10:02 +0200

    feat: add formatName function and output

--- jsx-syntax/src/App.jsx
+++ jsx-syntax/src/App.jsx
@@ -5,12 +5,22 @@ const world = 'World';
 
 const element = <h1>{hello} {world}</h1>;
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Tom',
+  lastName: 'Seidel'
+};
+
 function App() {
 
   return (
     <>
       <h1>{hello} {world}</h1>
       {element}
+      <h2>{formatName(user)}</h2>
     </>
   )
 }
